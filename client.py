1248c6db73913a352b1fbdc23265913e62c9521c
Abhilash Saj <abhilashsaj@gmail.com>
2020-05-02 12:39:04 +0530

    server client

--- client.py
+++ client.py
@@ -1,34 +1,11 @@
-import time, socket, sys
+import socket
 
-print("\nWelcome to Chat Room\n")
-print("Initialising....\n")
-time.sleep(1)
-
-s = socket.socket()
-shost = socket.gethostname()
-ip = socket.gethostbyname(shost)
-print(shost, "(", ip, ")\n")
-host = input(str("Enter server address: "))
-name = input(str("\nEnter your name: "))
-port = 1234
-print("\nTrying to connect to ", host, "(", port, ")\n")
-time.sleep(1)
+host = socket.gethostname()    
+port = 12345                   # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-print("Connected...\n")
-
-s.send(name.encode())
-s_name = s.recv(1024)
-s_name = s_name.decode()
-print(s_name, "has joined the chat room\nEnter [e] to exit chat room\n")
-
-while True:
-    message = s.recv(1024)
-    message = message.decode()
-    print(s_name, ":", message)
-    message = input(str("Me : "))
-    if message == "[e]":
-        message = "Left chat room!"
-        s.send(message.encode())
-        print("\n")
-        break
-    s.send(message.encode())
+c_msg = input("B: ")
+s.sendall(c_msg.encode('utf-8'))
+data = s.recv(1024)
+s.close()
+print('Received', repr(data))
--- server.py
+++ server.py
@@ -1,22 +1,15 @@
-import socket                
-  
-s = socket.socket()          
-print ("Socket successfully created")
-  
-port = 12345                
-  
+import socket
 
-s.bind(('', port))         
-print ("socket binded to %s" %(port) )
-
-s.listen(5)      
-print ("socket is listening" )           
-  
-while True: 
-
-   c, addr = s.accept()      
-   print ('Got connection from', addr )
-
-   c.send('Thank you for connecting') 
-
-   c.close() 
+host = ''        # Symbolic name meaning all available interfaces
+port = 12345     # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
+while True:
+    data = conn.recv(1024)
+    if not data: break
+    print(data.decode('utf-8'))
+    conn.sendall(b'Hello, world from server')
+conn.close()
