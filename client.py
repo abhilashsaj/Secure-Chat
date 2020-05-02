a742a3af7a4c357b5085694aee81820a83f47878
Abhilash Saj <abhilashsaj@gmail.com>
2020-05-02 15:04:43 +0530

    key transfer

--- client.py
+++ client.py
@@ -1,4 +1,11 @@
 import socket
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
 
 print("Client B started...")
 host = socket.gethostname()    
@@ -6,8 +13,9 @@ port = 12345                   # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
-data = s.recv(1024)
-print("Key: " + data.decode('utf-8'))
+key = s.recv(1024)
+f = Fernet(key)
+print(key)
 
 while(True):
 	c_msg = input("B: ")
