3c25ac6531ece3580cc58e5fb15f2fb64cac7718
Abhilash Saj <abhilashsaj@gmail.com>
2020-05-02 12:52:04 +0530

    completed chat between A and B

--- client.py
+++ client.py
@@ -4,8 +4,17 @@ host = socket.gethostname()
 port = 12345                   # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-c_msg = input("B: ")
-s.sendall(c_msg.encode('utf-8'))
-data = s.recv(1024)
+
+while(True):
+	c_msg = input("B: ")
+	s.sendall(c_msg.encode('utf-8'))
+
+	data = s.recv(1024)
+	print("A: " + data.decode('utf-8'))
+
+	if data.decode('utf-8') == "bye":
+	    break
+
+
+
 s.close()
-print('Received', repr(data))
--- server.py
+++ server.py
@@ -7,9 +7,26 @@ s.bind((host, port))
 s.listen(1)
 conn, addr = s.accept()
 print('Connected by', addr)
+
+
 while True:
     data = conn.recv(1024)
-    if not data: break
-    print(data.decode('utf-8'))
-    conn.sendall(b'Hello, world from server')
+    if not data: 
+    	break
+
+    print("B: "+data.decode('utf-8'))
+    
+    if data.decode('utf-8') == "bye":
+
+    	break
+
+    
+    
+
+    s_msg = input("A: ")
+    conn.sendall(s_msg.encode('utf-8'))
+    # if data == 'bye': 
+    # 	break
+
+	
 conn.close()
